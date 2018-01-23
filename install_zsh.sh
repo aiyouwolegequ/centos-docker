0ec4bb479432195b0ffdb6812670060321893f2b
aiyouwolegequ <aiyouwolegequa@gmail.com>
2018-01-23 20:41:34 +0800

    version 1.3

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#version 1.3
 
 yum install zsh -q -y
 git clone -q --depth=1 https://github.com/robbyrussell/oh-my-zsh.git /root/.oh-my-zsh
@@ -16,7 +17,7 @@ cat > /root/.zshrc <<-EOF
 export ZSH=/root/.oh-my-zsh
 ZSH_THEME="dracula"
 plugins=(sudo zsh-syntax-highlighting git autojump web-search zsh_reload colored-man-pages zsh-autosuggestions zsh-history-substring-search)
-source $ZSH/oh-my-zsh.sh
+source /root/.oh-my-zsh/oh-my-zsh.sh
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 alias vizsh="vim ~/.zshrc"
 alias sourcezsh="source ~/.zshrc"
